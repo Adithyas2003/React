4b6147062586ed84bc4c24308ddecf517f5bbed4
Adithyas2003 <Adathyas49@gmail.com>
2024-10-15 11:29:20 +0530

    new updated

---
+++ mynew_app/src/Component/Layout.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+function Layout(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home page</Link> 
+                </li>
+                <li>
+                    <Link to="/Contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default Layout
